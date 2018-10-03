ec87a709b1ebd191e12c4a2d77c227ce01f1cf0b
sespesogil <42008709+sespesogil@users.noreply.github.com>
2018-10-03 08:21:48 -0700

    Create split_mapper.sh

---
+++ split_mapper.sh
@@ -0,0 +1,36 @@
+varname=    #add the date (/mm/day/year) and ID run
+
+
+# listing all fastq files
+
+find `pwd` -name \*.fastq.gz -exec ls {} \; > list.$varname.ATACseq.txt
+
+find `pwd` -name \*.fastq.gz -exec dirname {} \; | sort | uniq > repository
+
+
+# creating folders
+
+dir=`pwd`
+mkdir $dir/mapping
+fastq=$(head repository)
+
+# gunzipping 
+
+tr '\n' '\0' < list.$varname.txt | xargs -0 -r gunzip --
+
+### split-mapping 
+
+
+cat filenames.txt | while read LINE; do 
+
+	bwa mem -t 4 $REF $fastq/"${LINE}".R1.fastq $fastq/"${LINE}".R2.fastq > $dir/mapping/$varname.sam
+	samtools view -b -S -o $dir/mapping/$varname.bam $dir/mapping/$varname.sam
+	samtools flagstat $dir/mapping/$varname.bam | sed -i '${LINE}' > $dir/mapping/$varname.txt
+	rm $dir/mapping/$varname.sam
+	; done
+
+
+cat $dir/mapping/*.txt > $dir/mapping/report.$varname.txt
+
+
+
