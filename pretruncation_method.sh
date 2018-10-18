5f2118a6a7586395d970d5e208edb05ac099868d
sespesogil <42008709+sespesogil@users.noreply.github.com>
2018-10-18 11:41:31 -0400

    Update pretruncation_method.sh

--- pretruncation_method.sh
+++ pretruncation_method.sh
@@ -1,10 +1,10 @@
 
 
-HICPRO=
-PATH=
-DIR=
+HICPRO=   #indicate installation directory
+PATH=     # path fastq files
+DIR=      # directory were results will be allocated 
 
-$HICPRO -i $PATH -o $DIR -c config_pretruncation.txt -p 
+$HICPRO -i $PATH -o $DIR -c config_file_pretruncation.txt -p 
 
 
 # launch resulting scripts 
